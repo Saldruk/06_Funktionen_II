ec3051c7be2183869a549b6fd4bfd103b31743e0
Saldruk <Divermeik@gmail.com>
2022-11-14 09:04:32 +0100

    Ausgabe und addieren + test

--- script_01.js
+++ script_01.js
@@ -5,6 +5,22 @@
 1. Dateneingabe + -überprüfung :
 2. Auswahl Rechenart : 
 3. Fkt. Grundrechenarten :
-4. Ausgabe in Konsole :
+4. Ausgabe in Konsole : check!
 */
 
+// module : addition a+b | test
+output(add(2,3));
+output(add(2,-3));
+output(add(2,0));
+function add(a,b) {
+    let c = a + b;
+    return c;   
+}
+
+
+// module: console ouput | test: 
+// output("hello");
+// output(2);
+function output(outputData) {
+    console.log(outputData);
+ }
